a68ca360d37b178c0f1e87a471ca30103bd3e300
76868 <107250796+76868@users.noreply.github.com>
2024-04-26 20:32:05 +0900

    Add files via upload

---
+++ bullethell2/main.js
@@ -0,0 +1,83 @@
+var assets = {
+    back: "images/back2.png",
+    player: "images/raiden3 k.png",
+    player2: "images/karijiki.png",
+    aaa: "images/ppl.png",
+    bullet: "images/wimn3.png",
+    enemybullet: "images/gba.png",
+    enemybullet2: "images/gba22.png",
+    enemybullet3: "images/aba2.png",
+    enemy1: "images/enemy1.png",
+    enemy2: "images/enemy2.png",
+    enemy3: "images/enemy3.5.png",
+    enemy31: "images/enemy3.2.png",
+    enemy32: "images/enemy3.3.png",
+    enemy33: "images/enemy3.4.png",
+    laser: "images/sslaser2.png",
+    lasero: "images/lasero69-21.png",
+    item: "images/item.png",
+    item2: "images/pt22.png",
+    map: "images/map.png",
+    ten: "sounds/ten.mp3",
+    op: "images/op.png",
+    lz: "images/wiez21.png",
+    mark: "images/mark.png",
+    aura: "images/baria.png",
+    lests: "images/zannki.png",
+    bomb: "images/bomb341-341.png",
+    // nidlbl: "images/nidlr2.png",
+    kome:"images/nidlr2.png",
+    aokome:"images/nidlr2br.png",
+    rba: "images/rba.png",
+    rba3: "images/rba3.png",
+    bblbig:"images/bblbig.png",
+    bbl:"images/gba9-9.png",
+    smrba:"images/smrba.png",
+};
+var core;
+
+enchant();
+
+window.onload = function () {
+
+    core = new Core(360, 480);
+
+
+    // core = new Core(560, 680);
+    core.fps = 60;
+    // setInterval(function(){
+       
+    //        if (core.fps <= 59) {
+    //     core.fps = 60;
+    // }},10
+
+    // )
+  
+
+    
+
+    core.preload(assets);
+    core.onload = function () {
+        titleStart();
+    };
+
+    // HACK: core.start を core.debug に変えてみましょう
+    // スプライトが赤く囲われて大きさがわかるようになります
+    // core.debug();
+    core.start();
+}
+
+// 配列の要素を繰り返す
+// var ary = [1,2,3].repeatOneByOne(3);
+// console.log(ary);
+// -> [1,1,1,2,2,2,3,3,3] 
+Array.prototype.repeatOneByOne = function (count) {
+    var ary = [];
+    var _ary = this;
+    while ((__ary = _ary.shift()) !== undefined) {
+        for (var i = 0; i < count; i++) {
+            ary.push(__ary);
+        }
+    }
+    return ary;
+}
